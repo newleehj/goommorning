aef57083dae9e0b39fa6b45fff923c76d57950e7
newleehj <humanwriting82@naver.com>
2020-02-23 18:58:22 +0900

    굼모닝소개 게시판 수정 -skin > intro 수정 -관련 이미지 업데이트

--- theme/basic/skin/board/intro/list.skin.php
+++ theme/basic/skin/board/intro/list.skin.php
@@ -13,14 +13,41 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
 ?>
 
 <section id="introwrap">
-    <article class="introimg">
-        <img src="<?php echo G5_URL ?>/images/intro1.png" alt="굼벵이 확대">
-    </article>
     <article class="introtext">
-        <p>Goom모닝은 고향인 전라남도 함평으로 내려온 굼아저씨와 잡초농부가<br>2018년, 굼벵이 농장을 짓고 흰점박이꽃무지(굼벵이) 사육을 시작했습니다.<br>
-        2019년부터 안정적인 생산에 맞춰 기업 중심으로 납품을 해왔고 <br>2020년부터 지속적인 생산량 증가로 온라인 판매까지 영역을 확장했습니다<br>
-        단백질의 보고이자 미래 식량으로 주목받는 굼벵이!<br>Goom모닝은 농촌 활력을 위해 도전하는 청년들의 농장입니다!<br>
-        현재 굼아저씨는 유채 재배, 잡초농부는 단삼과 삼잎국화도 재배도 하고 있답니다.</p> 
+            <div class="introradius"></div>
+            <div class="introimg1">
+                <img src="<?php echo G5_URL ?>/images/intro/intro1.png" alt="health">
+                <p><span>Health</span><br>고단백 굼벵이</p>
+            </div>
+            <div class="introimg2">
+                <img src="<?php echo G5_URL ?>/images/intro/intro2.png" alt="Pure">
+                <p><span>Pure</span><br>주기적인 식품검사</p>
+            </div>
+            <div class="introimg3">
+                <img src="<?php echo G5_URL ?>/images/intro/intro3.png" alt="Future">
+                <p><span>Future</span><br>미래 대안 식량</p>
+            </div>
+            <div class="introimg4">
+                <img src="<?php echo G5_URL ?>/images/intro/intro4.png" alt="굼아저씨">
+                <p><br><br>굼아저씨 이행섭</p>
+            </div>
+            <div class="introimg5">
+                <img src="<?php echo G5_URL ?>/images/intro/intro5.png" alt="잡초농부">
+                <p><br><br><br><br>잡초농부 이지형</p>
+            </div>
+            <div class="introimg6">
+                <img src="<?php echo G5_URL ?>/images/intro/intro6.png" alt="굼모닝 축사">
+                <p><span>Goom모닝</span><br>2018년 신축축사</p>
+            </div>
+            <div class="introimg7">
+               <img src="<?php echo G5_URL ?>/images/intro/intro7.png" alt="굼모닝 축사">
+               <p class="intro7text"> 
+               <span>Goom모닝</span>은<br>
+                청정한 전남 함평군에 있으며<br>
+                귀농한 두 청년농부가 아주 열심히<br>
+                흰점박이꽃무지(꽃벵이) 사육과<br>
+                그 유충인 굼벵이를 판매합니다<br></p>
+            </div>
     </article>
 </section>
 <section class="videowrap">
@@ -34,15 +61,14 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
         <p>▲ EBS 한국기행</p>
     </div>
 </section>
-<section class="podwrap">
-    <p>굼아저씨와 잡초농부의 팟캐스트</p>
-    <img src="<?php echo G5_URL ?>/images/pod.PNG" alt="팟캐스트 썸네일">
-    <a href="http://www.podbbang.com/ch/16646">팟캐스트 바로가기 Click >> </a>
-</section>
 <section class="mapwrap">
     <p class="title">Goom모닝 오시는 길</p>
-    <img src="<?php echo G5_URL ?>/images/intro2.png" alt="함께 찍은 사진">
-    <p>전라남도 함평군 해보면 올림픽로 973<br>(지번)해보면 대각리 623(우) 57115</p>
+    <img src="<?php echo G5_URL ?>/images/intro/intro8.jpg" alt="굼모닝 굼아저씩 잡초농부">
+    <p>·&nbsp;주소 : 전라남도 함평군 해보면 올림픽로 973 (우)57115<br><br>
+            ·&nbsp;휴대전화 : 010-5904-1603</br><br>
+            ·&nbsp;굼모닝 농장 : 061-000-0000</br><br>
+            ·&nbsp;Fax : 0504-333-5627<br><br>
+            ·&nbsp;E-mail : goommorning@naver.com</p>
 </section>
 
 
